55712b896fa2ca87af8082f1e93d5edb375ccfe9
devSaifur <saifur2akash@gmail.com>
2024-11-14 01:18:18 +0600

    nodes done

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode, TextType
 
 
 def main():
-    text_node = TextNode("Hello World", TextType.normal, "https://www.google.com")
+    text_node = TextNode("Hello World", TextType.TEXT, "https://www.google.com")
     print(text_node)
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,22 +1,50 @@
 from enum import Enum
+from htmlnode import LeafNode
 
-TextType = Enum("TextType", ["normal", "bold", "italic", "code", "links", "images"])
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
-    def __init__(self, text, text_type: Enum, url=None) -> None:
+    def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = TextType(text_type)
+        self.text_type = text_type
         self.url = url
 
-    def __eq__(self, value: object) -> bool:
-        if isinstance(value, TextNode):
-            return (
-                self.text == value.text
-                and self.text_type == value.text_type
-                and self.url == value.url
-            )
-        return False
+    def __eq__(self, other):
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    text = text_node.text
+    url = text_node.url
+    text_type = text_node.text_type
+
+    match text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=text)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text)
+        case TextType.LINK:
+            return LeafNode(tag="a", value=text, props={"href": url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": url, "alt": text})
+        case _:
+            raise ValueError(f"Unsupported TextType: {text_type}")
