3ea69f30f09ff17ce2f2934a5c1dbdb192569bec
devSaifur <saifur2akash@gmail.com>
2024-11-15 23:36:37 +0600

    handle blocks done

---
+++ test.py
@@ -0,0 +1,9 @@
+hello = ["hello, world!", "hello, world!", "hello, world!"]
+
+for i, h in enumerate(hello):
+    print(f"hello {i}: {h}")
+
+
+block = "1. Item 1\n2. Item 2\n3. Item 3"
+
+print(block)
