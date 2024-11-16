3b7e43048b585bcefa0d0443a41e981cc47a319a
devSaifur <saifur2akash@gmail.com>
2024-11-16 23:03:58 +0600

    gen pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,46 @@
-def extract_md(md: str):
+import os
+from pathlib import Path
+from block_markdown import markdown_to_html_node
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
+def generate_page(from_path: Path, template_path: Path, dest_path: Path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    from_file = open(from_path, "r")
+    markdown = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    html = markdown_to_html_node(markdown).to_html()
+    html_title = extract_title(markdown) or "Untitled"
+
+    template = template.replace("{{ Title }}", html_title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path, exist_ok=True)
+
+    dest_file = open(dest_path, "w")
+    dest_file.write(template)
+    dest_file.close()
+
+
+def extract_title(md: str):
     lines = md.split("\n")
 
     for line in lines:
--- src/test_gencontent.py
+++ src/test_gencontent.py
@@ -1,16 +1,16 @@
 import unittest
-from gencontent import extract_md
+from gencontent import extract_title
 
 
 class TestGenContent(unittest.TestCase):
     def test_gen_content(self):
         md = "# Hello, world!\n This is a paragraph. \n\n"
-        self.assertEqual(extract_md(md), "Hello, world!")
+        self.assertEqual(extract_title(md), "Hello, world!")
 
     def test_gen_content_no_title(self):
         md = "This is a paragraph\n This is a subheading"
         with self.assertRaises(ValueError):
-            extract_md(md)
+            extract_title(md)
 
 
 if __name__ == "__main__":
